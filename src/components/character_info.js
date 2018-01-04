a722b09e338195a93ce46bea7c672d294b325617
Andrew <andrew.vinocur@gmail.com>
2018-01-03 21:42:51 -0800

    creation of components folder

---
+++ src/components/character_info.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class CharacterInfo extends Component {
+
+  render() {
+    return(
+      <div>
+        <h1>Name</h1>
+        <h1>Description</h1>
+        <h1>Refresh</h1>
+      </div>
+    )
+  }
+}
+
+export default CharacterInfo;
