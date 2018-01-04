d10bc7d21aefe1381ef8e0f5390f663a74af6f83
Andrew <andrew.vinocur@gmail.com>
2018-01-03 17:34:15 -0800

    src folder that will contain app entry and components

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+
+  render() {
+    return(
+      <div>
+        <h1>Test works!</h1>
+      </div>
+    )
+  }
+}
+
+
+ReactDOM.render(<App />, document.querySelector('.container'));
