58af4c635eade439fa2b56f5ccaaf558c033d3b7
Andrew <andrew.vinocur@gmail.com>
2018-01-03 17:58:15 -0800

    technically, this config works but need to learn how to manually configure these files

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+
+module.exports = {
+
+    entry: path.resolve(__dirname, 'src') + '/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist') + '/app',
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src'),
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'env']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            }
+        ]
+    }
+};
