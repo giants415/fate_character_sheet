68f35b2f69278ffa880bc9e32beda942f21ce901
Andrew <andrew.vinocur@gmail.com>
2018-01-03 21:42:37 -0800

    imports character info component and adds to App

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import CharacterInfo from './components/character_info';
+
 class App extends Component {
 
   render() {
     return(
       <div>
-
+        <CharacterInfo />
       </div>
     )
   }
