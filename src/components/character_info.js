cf6c8b9c6ba0770dade17b226c0d01cf034add01
Andrew <andrew.vinocur@gmail.com>
2018-01-04 16:05:59 -0800

    refactored to a functional component since name component is gone

--- src/components/character_info.js
+++ src/components/character_info.js
@@ -2,16 +2,14 @@ import React, { Component } from 'react';
 
 import Name from './name';
 
-class CharacterInfo extends Component {
-  render() {
-    return(
-      <div>
-        <Name />
-        <h1>Description</h1>
-        <h1>Refresh</h1>
-      </div>
-    )
-  }
-}
+const CharacterInfo = (props) => {
+  return (
+    <div>
+      <h1>Name: {props.myChar.name}</h1>
+      <h4>Description: {props.myChar.description}</h4>
+      <h4>Refresh: {props.myChar.refresh}</h4>
+    </div>
+  )
+};
 
 export default CharacterInfo;
