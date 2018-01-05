61d590ec89fe689375074da03b802639158057fd
Andrew <andrew.vinocur@gmail.com>
2018-01-04 16:40:02 -0800

    refacted state to take in a character from an array of objs.
    myChar state drills into object, directly to sheet

--- src/index.js
+++ src/index.js
@@ -3,21 +3,28 @@ import ReactDOM from 'react-dom';
 
 import CharacterInfo from './components/character_info';
 
+const characterList = [
+  {
+    player: 'me',
+    characterSheet: {
+      name: 'garfinkle',
+      description: 'a cool guy with a disposition',
+      refresh: 3
+    }
+  }
+]
+
 class App extends Component {
   constructor(props){
     super(props);
 
-    this.state = {
-      name: 'garfinkle',
-      description: 'a cool guy with a disposition',
-      refresh: 3
-    };
+    this.state = characterList[0];
   }
 
   render() {
     return(
       <div>
-        <CharacterInfo myChar={this.state}/>
+        <CharacterInfo myChar={this.state.characterSheet}/>
       </div>
     )
   }
