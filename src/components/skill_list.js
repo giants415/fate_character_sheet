eb96cac1c5d6e40c9ba4e0e124e8d9d6a8fd5e10
Andrew <andrew.vinocur@gmail.com>
2018-01-06 12:34:19 -0800

    imports and passes data for great skills component

--- src/components/skill_list.js
+++ src/components/skill_list.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import SuperbSkills from './skill_rank_components/superb_skills';
+import GreatSkills from './skill_rank_components/great_skills';
+
 
 const SkillList = ({mySkills}) => {
   return (
     <div>
       <SuperbSkills superbSkills={mySkills.superb}/>
+      <GreatSkills greatSkills={mySkills.great}/>
     </div>
   );
 }
