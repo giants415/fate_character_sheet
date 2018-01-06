c67947ef5d5a68e4d4e311b81c4f85cce89838c4
Andrew <andrew.vinocur@gmail.com>
2018-01-06 11:53:05 -0800

    adds aspect title h4

--- src/components/aspect_list.js
+++ src/components/aspect_list.js
@@ -3,6 +3,7 @@ import React from 'react';
 const AspectList = ({myAspect}) => {
   return (
     <div>
+      <h4>Aspects</h4>
       <p>{myAspect[0]}</p>
       <p>{myAspect[1]}</p>
       <p>{myAspect[2]}</p>
