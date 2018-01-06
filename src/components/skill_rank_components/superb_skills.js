5c3a2b22ba672e3c2118fb817022f72ac428dd22
Andrew <andrew.vinocur@gmail.com>
2018-01-06 11:54:02 -0800

    superb component lists all skills

--- src/components/skill_rank_components/superb_skills.js
+++ src/components/skill_rank_components/superb_skills.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const SuperbSkills = () => {
+const SuperbSkills = ({superbSkills}) => {
   return (
     <div>
-      
+      <h4>Superb</h4>
+      <p>{superbSkills[0]}</p>
+      <p>{superbSkills[1]}</p>
+      <p>{superbSkills[2]}</p>
+      <p>{superbSkills[3]}</p>
+      <p>{superbSkills[4]}</p>
     </div>
   );
 }
