75fd7a9b6c01c1b7cd823b6606fc36dc85aaaf32
Andrew <andrew.vinocur@gmail.com>
2018-01-06 12:40:56 -0800

    completes fair skill component

---
+++ src/components/skill_rank_components/fair_skills.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const FairSkills = ({fairSkills}) => {
+  return (
+    <div>
+      <h4>Fair</h4>
+      <p>{fairSkills[0]}</p>
+      <p>{fairSkills[1]}</p>
+      <p>{fairSkills[2]}</p>
+      <p>{fairSkills[3]}</p>
+      <p>{fairSkills[4]}</p>
+    </div>
+  );
+}
+
+export default FairSkills;
