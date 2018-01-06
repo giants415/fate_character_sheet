8444abe2aede22e0cb290fdd626775f87e4c2686
Andrew <andrew.vinocur@gmail.com>
2018-01-06 12:42:45 -0800

    imports and passes data for average skills component

--- src/components/skill_list.js
+++ src/components/skill_list.js
@@ -4,6 +4,8 @@ import SuperbSkills from './skill_rank_components/superb_skills';
 import GreatSkills from './skill_rank_components/great_skills';
 import GoodSkills from './skill_rank_components/good_skills';
 import FairSkills from './skill_rank_components/fair_skills';
+import AverageSkills from './skill_rank_components/average_skills';
+
 
 
 
@@ -15,6 +17,7 @@ const SkillList = ({mySkills}) => {
       <GreatSkills greatSkills={mySkills.great}/>
       <GoodSkills goodSkills={mySkills.good}/>
       <FairSkills fairSkills={mySkills.fair}/>
+      <AverageSkills AverageSkills={mySkills.average}/>
     </div>
   );
 }
