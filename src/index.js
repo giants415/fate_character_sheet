5d817182cff87b7bf724327a88113e53d19c923d
Andrew <andrew.vinocur@gmail.com>
2018-01-06 11:32:41 -0800

    adds placeholder skill list data

--- src/index.js
+++ src/index.js
@@ -18,6 +18,13 @@ const characterList = [
         3: 'aspect 3',
         4: 'aspect 4',
         5: 'aspect 5',
+      },
+      skillList: {
+        superb: {1: 'superb 1', 2: 'superb 2', 3: 'superb 3', 4: 'superb 4', 5: 'superb 5'},
+        great: {1: 'great 1', 2: 'great 2', 3: 'great 3', 4: 'great 4', 5: 'great 5'},
+        good: {1: 'good 1', 2: 'good 2', 3: 'good 3', 4: 'good 4', 5: 'good 5'},
+        fair: {1: 'fair 1', 2: 'fair 2', 3: 'fair 3', 4: 'fair 4', 5: 'fair 5'},
+        average: {1: 'average 1', 2: 'average 2', 3: 'average 3', 4: 'average 4', 5: 'average 5'},
       }
     }
   }
