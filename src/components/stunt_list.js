db110cc2f28120891b54d287ce6712d7e32f4df8
Andrew <andrew.vinocur@gmail.com>
2018-01-07 17:03:48 -0800

    finishes stunt list component

---
+++ src/components/stunt_list.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const StuntList = ({myStunts}) => {
+  return (
+    <div>
+      <h4>Stunts</h4>
+      <p>{myStunt[0]}</p>
+      <p>{myStunt[1]}</p>
+    </div>
+  )
+};
+
+export default StuntList;
