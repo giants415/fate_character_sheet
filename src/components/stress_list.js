f3ddccbbdcc5710e405217b04615875ff542c2c8
Andrew <andrew.vinocur@gmail.com>
2018-01-10 15:39:46 -0800

    imports and passes data to physical stress component

--- src/components/stress_list.js
+++ src/components/stress_list.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import MentalStressItems from './mental_item';
+import PhysicalStressItems from './physical_item';
 
 const StressList = ({myStress}) => {
   return (
     <div>
       <h4>Stress Categories</h4>
       <MentalStressItems mental={myStress.mental}/>
+      <PhysicalStressItems physical={myStress.physical}/>
     </div>
   )
 };
