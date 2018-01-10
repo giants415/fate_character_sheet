39b45a663a23c2c3695172a74a1843b8a9fac3d1
Andrew <andrew.vinocur@gmail.com>
2018-01-10 15:15:19 -0800

    added a title to denote it in app during build

--- src/components/stress_list.js
+++ src/components/stress_list.js
@@ -3,6 +3,7 @@ import React from 'react';
 const StressList = ({myStress}) => {
   return (
     <div>
+      <h4>Stress Categories</h4>
       <PhysicalStress physical={myStress.physical}/>
       <MentalStress mental={myStress.mental}/>
     </div>
