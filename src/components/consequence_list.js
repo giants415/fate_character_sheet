d9e27b69c2c605606c545bdf969e9cd483877e64
Andrew <andrew.vinocur@gmail.com>
2018-01-10 15:49:38 -0800

    completes consequence list component

---
+++ src/components/consequence_list.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ConsequenceList = ({myConsequences}) => {
+  console.log(myConsequences);
+  return (
+    <div>
+      <h4>Consequences</h4>
+      <p>Mild: {myConsequences.mild}</p>
+      <p>Moderate: {myConsequences.moderate}</p>
+      <p>Severe: {myConsequences.severe}</p>
+    </div>
+  )
+};
+
+export default ConsequenceList;
