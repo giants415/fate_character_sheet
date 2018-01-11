14b151a95e8ac102dbd14dc8576f4fc6acebb4c9
Andrew <andrew.vinocur@gmail.com>
2018-01-10 16:27:41 -0800

    imports style sheet and gives a class name to name element

--- src/components/character_info.js
+++ src/components/character_info.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import '../../style/character_style.sass';
+
 const CharacterInfo = ({myChar}) => {
   return (
     <div>
-      <h1>Name: {myChar.name}</h1>
+      <h1 className='name'>Name: {myChar.name}</h1>
       <h4>Description: {myChar.description}</h4>
       <h4>Refresh: {myChar.refresh}</h4>
     </div>
