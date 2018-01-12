20c9f89edd0ca05044bd0200d7f55c011621fff8
Andrew <andrew.vinocur@gmail.com>
2018-01-11 23:32:27 -0800

    cleaning up organization

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,36 @@
 var path = require('path');
 
 module.exports = {
-
-    entry: path.resolve(__dirname, 'src') + '/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist') + '/app',
-        filename: 'bundle.js',
-        publicPath: '/'
-    },
-    module: {
-      loaders: [
-        {
-            test: /\.js$/,
-            include: path.resolve(__dirname, 'src'),
-            loader: 'babel-loader',
-            query: {
-                presets: ['react', 'env']
-            }
-        },
-        {
-          test: /\.scss$/,
-          use: extractSass.extract({
-            use: [{
-                loader: "css-loader"
-            }, {
-                loader: "sass-loader"
-            }],
-            fallback: "style-loader"
-          })
-        }
-      ]
-    }
-    plugins: [
-      extractSass
+  entry: path.resolve(__dirname, 'src') + '/index.js',
+  output: {
+      path: path.resolve(__dirname, 'dist') + '/app',
+      filename: 'bundle.js',
+      publicPath: '/'
+  },
+  module: {
+    loaders: [
+      {
+          test: /\.js$/,
+          include: path.resolve(__dirname, 'src'),
+          loader: 'babel-loader',
+          query: {
+              presets: ['react', 'env']
+          }
+      },
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+          use: [{
+              loader: "css-loader"
+          }, {
+              loader: "sass-loader"
+          }],
+          fallback: "style-loader"
+        })
+      }
     ]
+  }
+  plugins: [
+    extractSass
+  ]
 };
