b13c93c7f21223d61e3202236b539ff679ae303a
Andrew <andrew.vinocur@gmail.com>
2018-01-20 12:12:19 -0800

    fixes aspect list numbers from array

--- src/components/aspect_list.js
+++ src/components/aspect_list.js
@@ -6,10 +6,10 @@ const AspectList = ({myAspects}) => {
       <thead><tr><th>Aspects</th></tr></thead>
       <tbody>
         <tr><td>{myAspects[0]}</td></tr>
-        <tr><td>{myAspects[0]}</td></tr>
-        <tr><td>{myAspects[0]}</td></tr>
-        <tr><td>{myAspects[0]}</td></tr>
-        <tr><td>{myAspects[0]}</td></tr>
+        <tr><td>{myAspects[1]}</td></tr>
+        <tr><td>{myAspects[2]}</td></tr>
+        <tr><td>{myAspects[3]}</td></tr>
+        <tr><td>{myAspects[4]}</td></tr>
       </tbody>
     </table>
   )
