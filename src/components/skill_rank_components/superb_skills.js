90676b62a5b6a07e93a8d2e106206e8221f40c85
Andrew <andrew.vinocur@gmail.com>
2018-01-19 19:40:55 -0800

    creates inline ul and li styles, applies to elements

--- src/components/skill_rank_components/superb_skills.js
+++ src/components/skill_rank_components/superb_skills.js
@@ -1,15 +1,25 @@
 import React from 'react';
 
+const ulStyle = {
+  listStyleType: 'none',
+  paddingLeft: '0px'
+};
+
+const liStyle = {
+  display: 'inline',
+  margin: '10px'
+}
+
 const SuperbSkills = ({superbSkills}) => {
   return (
     <div>
       <h4>Superb</h4>
-      <ul>
-        <li>{superbSkills[0]}</li>
-        <li>{superbSkills[1]}</li>
-        <li>{superbSkills[2]}</li>
-        <li>{superbSkills[3]}</li>
-        <li>{superbSkills[4]}</li>
+      <ul style={ulStyle}>
+        <li style={liStyle}>{superbSkills[0]}</li>
+        <li style={liStyle}>{superbSkills[1]}</li>
+        <li style={liStyle}>{superbSkills[2]}</li>
+        <li style={liStyle}>{superbSkills[3]}</li>
+        <li style={liStyle}>{superbSkills[4]}</li>
       </ul>
     </div>
   );
