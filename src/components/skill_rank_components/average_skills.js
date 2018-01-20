7ecf2c7d9ec6f4c9393f645b1c874a6faf981c61
Andrew <andrew.vinocur@gmail.com>
2018-01-19 19:46:24 -0800

    adds inline aspect style and converts to a list

--- src/components/skill_rank_components/average_skills.js
+++ src/components/skill_rank_components/average_skills.js
@@ -1,14 +1,26 @@
 import React from 'react';
 
+const ulStyle = {
+  listStyleType: 'none',
+  paddingLeft: '0px'
+};
+
+const liStyle = {
+  display: 'inline',
+  margin: '10px'
+};
+
 const AverageSkills = ({AverageSkills}) => {
   return (
     <div>
       <h4>Average</h4>
-      <p>{AverageSkills[0]}</p>
-      <p>{AverageSkills[1]}</p>
-      <p>{AverageSkills[2]}</p>
-      <p>{AverageSkills[3]}</p>
-      <p>{AverageSkills[4]}</p>
+      <ul style={ulStyle}>
+        <li style={liStyle}>{AverageSkills[0]}</li>
+        <li style={liStyle}>{AverageSkills[1]}</li>
+        <li style={liStyle}>{AverageSkills[2]}</li>
+        <li style={liStyle}>{AverageSkills[3]}</li>
+        <li style={liStyle}>{AverageSkills[4]}</li>
+      </ul>
     </div>
   );
 }
