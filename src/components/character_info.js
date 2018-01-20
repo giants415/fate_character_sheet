be71621388055b50d5c9eafcfbfee83dcc7ec75c
Andrew <andrew.vinocur@gmail.com>
2018-01-20 12:25:29 -0800

    changes bootstrap classes to push description under name

--- src/components/character_info.js
+++ src/components/character_info.js
@@ -5,9 +5,13 @@ import React from 'react';
 const CharacterInfo = ({myChar}) => {
   return (
     <div>
-      <h1 className="col-lg-4">Name: {myChar.name}</h1>
-      <h4 className="col-lg-4">Description: {myChar.description}</h4>
-      <h4 className="col-lg-4">Refresh: {myChar.refresh}</h4>
+      <div className="col-sm-8">
+        <h1 className="col-6">Name: {myChar.name}</h1>
+        <h4 className="col-6">Description: {myChar.description}</h4>
+      </div>
+      <div className="col-lg-4">
+        <h4>Refresh: {myChar.refresh}</h4>
+      </div>
     </div>
   )
 };
