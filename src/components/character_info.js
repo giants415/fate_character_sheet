b8ee82b1004b727fbabd75587bc9bbdadbadf6a7
Andrew <andrew.vinocur@gmail.com>
2018-01-19 16:52:11 -0800

    cleaning up

--- src/components/character_info.js
+++ src/components/character_info.js
@@ -5,7 +5,7 @@ import React from 'react';
 const CharacterInfo = ({myChar}) => {
   return (
     <div>
-      <h1> Name: {myChar.name}</h1>
+      <h1>Name: {myChar.name}</h1>
       <h4>Description: {myChar.description}</h4>
       <h4>Refresh: {myChar.refresh}</h4>
     </div>
