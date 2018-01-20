4e3d234aaa58a69df91424c2f00658f0949cdb92
Andrew <andrew.vinocur@gmail.com>
2018-01-19 19:46:27 -0800

    adds inline aspect style and converts to a list

--- src/components/skill_rank_components/fair_skills.js
+++ src/components/skill_rank_components/fair_skills.js
@@ -1,14 +1,26 @@
 import React from 'react';
 
+const ulStyle = {
+  listStyleType: 'none',
+  paddingLeft: '0px'
+};
+
+const liStyle = {
+  display: 'inline',
+  margin: '10px'
+};
+
 const FairSkills = ({fairSkills}) => {
   return (
     <div>
       <h4>Fair</h4>
-      <p>{fairSkills[0]}</p>
-      <p>{fairSkills[1]}</p>
-      <p>{fairSkills[2]}</p>
-      <p>{fairSkills[3]}</p>
-      <p>{fairSkills[4]}</p>
+      <ul style={ulStyle}>
+        <li style={liStyle}>{fairSkills[0]}</li>
+        <li style={liStyle}>{fairSkills[1]}</li>
+        <li style={liStyle}>{fairSkills[2]}</li>
+        <li style={liStyle}>{fairSkills[3]}</li>
+        <li style={liStyle}>{fairSkills[4]}</li>
+      </ul>
     </div>
   );
 }
