ea3376ee16f0c7933bfe10d885072295c1af0787
Andrew <andrew.vinocur@gmail.com>
2018-01-19 19:46:30 -0800

    adds inline aspect style and converts to a list

--- src/components/skill_rank_components/good_skills.js
+++ src/components/skill_rank_components/good_skills.js
@@ -1,14 +1,26 @@
 import React from 'react';
 
+const ulStyle = {
+  listStyleType: 'none',
+  paddingLeft: '0px'
+};
+
+const liStyle = {
+  display: 'inline',
+  margin: '10px'
+};
+
 const GoodSkills = ({goodSkills}) => {
   return (
     <div>
       <h4>Good</h4>
-      <p>{goodSkills[0]}</p>
-      <p>{goodSkills[1]}</p>
-      <p>{goodSkills[2]}</p>
-      <p>{goodSkills[3]}</p>
-      <p>{goodSkills[4]}</p>
+      <ul style={ulStyle}>
+        <li style={liStyle}>{goodSkills[0]}</li>
+        <li style={liStyle}>{goodSkills[1]}</li>
+        <li style={liStyle}>{goodSkills[2]}</li>
+        <li style={liStyle}>{goodSkills[3]}</li>
+        <li style={liStyle}>{goodSkills[4]}</li>
+      </ul>
     </div>
   );
 }
