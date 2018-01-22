22b0f4de5cb8d016d9e0e901ebf06b975661341f
Andrew <andrew.vinocur@gmail.com>
2018-01-21 20:45:18 -0800

    fixes typo

--- src/components/stress_list.js
+++ src/components/stress_list.js
@@ -6,7 +6,7 @@ import PhysicalStressItems from './stress_components/physical_item';
 const StressList = ({myStress}) => {
   return (
     <div className="row">
-      <div classNam="col-sm-8">
+      <div className="col-sm-8">
         <h4>Stress Categories</h4>
         <MentalStressItems className="col-8" mental={myStress.mental}/>
         <PhysicalStressItems className="col-8" physical={myStress.physical}/>
