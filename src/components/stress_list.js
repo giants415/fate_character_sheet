11884ca668cae5ffdd4b9117da914da6a3bbcac1
Andrew <andrew.vinocur@gmail.com>
2018-01-21 21:19:56 -0800

    adds nogutters styling

--- src/components/stress_list.js
+++ src/components/stress_list.js
@@ -3,9 +3,14 @@ import React from 'react';
 import MentalStressItems from './stress_components/mental_item';
 import PhysicalStressItems from './stress_components/physical_item';
 
+const noGutters = {
+    paddingRight: '0',
+    paddingLeft: '0'
+};
+
 const StressList = ({myStress}) => {
   return (
-    <div className="col-sm-8">
+    <div className="col-sm-8" style={noGutters}>
       <h4>Stress Categories</h4>
       <MentalStressItems className="col-sm-8" mental={myStress.mental}/>
       <PhysicalStressItems className="col-sm-8" physical={myStress.physical}/>
