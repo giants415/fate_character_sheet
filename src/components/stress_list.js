004308a86ceeaae5013c2025ceeda18aeba1cf32
Andrew <andrew.vinocur@gmail.com>
2018-01-21 20:51:45 -0800

    removes containing div row

--- src/components/stress_list.js
+++ src/components/stress_list.js
@@ -5,12 +5,10 @@ import PhysicalStressItems from './stress_components/physical_item';
 
 const StressList = ({myStress}) => {
   return (
-    <div className="row">
-      <div className="col-sm-8">
-        <h4>Stress Categories</h4>
-        <MentalStressItems className="col-sm-8" mental={myStress.mental}/>
-        <PhysicalStressItems className="col-sm-8" physical={myStress.physical}/>
-      </div>
+    <div className="col-sm-8">
+      <h4>Stress Categories</h4>
+      <MentalStressItems className="col-sm-8" mental={myStress.mental}/>
+      <PhysicalStressItems className="col-sm-8" physical={myStress.physical}/>
     </div>
   )
 };
