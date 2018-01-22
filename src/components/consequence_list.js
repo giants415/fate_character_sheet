3afeba7a4c4e826c9a5d9da4a07a15c7efa60cbd
Andrew <andrew.vinocur@gmail.com>
2018-01-21 21:19:48 -0800

    adds nogutters styling

--- src/components/consequence_list.js
+++ src/components/consequence_list.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
+const noGutters = {
+    paddingRight: '0',
+    paddingLeft: '0'
+};
+
 const ConsequenceList = ({myConsequences}) => {
   return (
-    <div className="col-sm-4">
+    <div className="col-sm-4" style={noGutters}>
       <h4>Consequences</h4>
       <p>Mild: {myConsequences.mild}</p>
       <p>Moderate: {myConsequences.moderate}</p>
