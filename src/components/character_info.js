dd7067923ddccc48d8e5bcf3666378620ba1ef73
Andrew <andrew.vinocur@gmail.com>
2018-01-25 10:27:53 -0800

    1) imports character 0 while building inputs for character info component
    2) refactors code to use input onchange before onchange fn is created

--- src/components/character_info.js
+++ src/components/character_info.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
 
+import Character_0 from '../Character_0';
+
 class CharacterInfo extends Component {
   constructor(props){
     super(props);
-    
 
+    this.state = { term: '' };
   }
 
   render() {
     return (
       <div>
         <div className="col-sm-8">
-          <h1 className="col-6">Name: {myChar.name}</h1>
-          <h4 className="col-6">Description: {myChar.description}</h4>
+          <label> Name
+            <input
+              className="col-6"
+              value={this.state.term}
+              onChange={event => this.onInputChange(event.target.value)}
+              />
+          </label>
+          <h4 className="col-6">Description: {Character_0.characterSheet.description}</h4>
         </div>
         <div className="col-lg-4">
-          <h4>Refresh: {myChar.refresh}</h4>
+          <h4>Refresh: {Character_0.characterSheet.refresh}</h4>
         </div>
       </div>
     )
