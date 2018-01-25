2dacb325a96970818ea9f4a2b273a919892f8324
Andrew <andrew.vinocur@gmail.com>
2018-01-25 10:55:20 -0800

    adds resources for when I build this tomorrow

---
+++ src/components/text_lock.js
@@ -0,0 +1,8 @@
+// resources for building
+//
+// https://stackoverflow.com/questions/36773671/deactivate-input-in-react-with-a-button-click
+//
+// https://stackoverflow.com/questions/13701923/disable-editing-default-value-of-text-input
+//
+// https://stackoverflow.com/questions/9649877/how-to-disable-enable-input-field-on-click-with-jquery
+// probably the best source for a simple button
