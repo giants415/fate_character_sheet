fc72c0eb4681d4143246470e39f0089e3d126f62
Andrew <andrew.vinocur@gmail.com>
2018-01-25 10:19:22 -0800

    begins changing component over to class based

--- src/components/character_info.js
+++ src/components/character_info.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const CharacterInfo = ({myChar}) => {
-  return (
-    <div>
-      <div className="col-sm-8">
-        <h1 className="col-6">Name: {myChar.name}</h1>
-        <h4 className="col-6">Description: {myChar.description}</h4>
+class CharacterInfo extends Component {
+  constructor(props){
+    super(props);
+    
+
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="col-sm-8">
+          <h1 className="col-6">Name: {myChar.name}</h1>
+          <h4 className="col-6">Description: {myChar.description}</h4>
+        </div>
+        <div className="col-lg-4">
+          <h4>Refresh: {myChar.refresh}</h4>
+        </div>
       </div>
-      <div className="col-lg-4">
-        <h4>Refresh: {myChar.refresh}</h4>
-      </div>
-    </div>
-  )
+    )
+  }
+
 };
 
 export default CharacterInfo;
