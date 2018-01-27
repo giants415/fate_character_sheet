f1f4d0510abb2db7cc52250c9845e193696bf5bd
Andrew <andrew.vinocur@gmail.com>
2018-01-26 16:16:52 -0800

    creates description component

---
+++ src/components/description.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class Description extends Component {
+  constructor(props){
+    super(props);
+    this.state = { name: '' };
+  }
+
+  render() {
+    return (
+      <div className="col-sm-8">
+        <label> Name
+          <input
+            className="col-6"
+            value={this.state.description}
+            onChange={event => this.onInputChange(event.target.value)}
+            />
+        </label>
+      </div>
+    );
+  }
+
+  onInputChange(description) {
+    this.setState({description});
+  }
+}
+
+export default Description;
