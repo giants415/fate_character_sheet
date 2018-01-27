5c50fe21a591953bfaa9bd92740baaed42c168bb
Andrew <andrew.vinocur@gmail.com>
2018-01-26 16:09:54 -0800

    refactors to remove description and refresh to be put into sep files

--- src/components/character_info.js
+++ src/components/character_info.js
@@ -6,43 +6,24 @@ class CharacterInfo extends Component {
   constructor(props){
     super(props);
 
-    this.state = {
-      name: '',
-      description: '',
-      refresh: ''
-     };
-  }
+    this.state = { name: '' };
 
   render() {
     return (
-      <div>
-        <div className="col-sm-8">
-          <label> Name
-            <input
-              className="col-6"
-              value={this.state.term}
-              onChange={event => this.onInputChange(event.target.value)}
-              />
-          </label>
+      <div className="col-sm-8">
+        <label> Name
           <input
             className="col-6"
-            value={this.state.term}
-            onChange={event => this.onInputChange(event.target.value)}
-            />        </div>
-        <div className="col-lg-4">
-          <input
-            className="col-6"
-            value={this.state.term}
+            value={this.state.name}
             onChange={event => this.onInputChange(event.target.value)}
             />
-        </div>
+        </label>
       </div>
-    )
+    );
   }
 
-  onInputChange(term) {
-    console.log(term);
-    this.setState({term});
+  onInputChange(namw) {
+    this.setState({name});
   }
 };
 
