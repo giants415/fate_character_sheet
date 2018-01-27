151d2255a22c909e4b72facdd34073438f5d00b8
Andrew <andrew.vinocur@gmail.com>
2018-01-26 16:52:43 -0800

    removes row

--- src/index.js
+++ src/index.js
@@ -19,10 +19,7 @@ class App extends Component {
   render() {
     return(
       <div>
-
-        <div className="row">
           <CharacterList />
-        </div>
 
         <div className="row">
           <AspectList myAspects={this.state.characterSheet.aspectList}/>
