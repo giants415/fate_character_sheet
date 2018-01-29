caf2d434063180077f13efc9a7ffeb42377603f9
Andrew <andrew.vinocur@gmail.com>
2018-01-29 14:33:12 -0800

    moves table inside of div with bootstrap

--- src/components/aspect_list.js
+++ src/components/aspect_list.js
@@ -1,22 +1,23 @@
 import React from 'react';
 
 const tableStyle = {
-  maxWidth: '50%',
   border: '1px black solid'
 };
 
 const AspectList = ({myAspects}) => {
   return (
-    <table style={tableStyle} className="col-lg-6 table">
-      <thead><tr><th>Aspects</th></tr></thead>
-      <tbody>
-        <tr><td>{myAspects[0]}</td></tr>
-        <tr><td>{myAspects[1]}</td></tr>
-        <tr><td>{myAspects[2]}</td></tr>
-        <tr><td>{myAspects[3]}</td></tr>
-        <tr><td>{myAspects[4]}</td></tr>
-      </tbody>
-    </table>
+    <div className="col-lg-6">
+      <table style={tableStyle} className="table">
+        <thead><tr><th>Aspects</th></tr></thead>
+        <tbody>
+          <tr><td>{myAspects[0]}</td></tr>
+          <tr><td>{myAspects[1]}</td></tr>
+          <tr><td>{myAspects[2]}</td></tr>
+          <tr><td>{myAspects[3]}</td></tr>
+          <tr><td>{myAspects[4]}</td></tr>
+        </tbody>
+      </table>
+    </div>
   )
 };
 
