56bcb4b1e6bd383054092c0caecf776de9f92c26
Andrew <andrew.vinocur@gmail.com>
2018-01-29 14:32:29 -0800

    removes styling

--- src/components/stress_list.js
+++ src/components/stress_list.js
@@ -3,14 +3,9 @@ import React from 'react';
 import MentalStressItems from './stress_components/mental_item';
 import PhysicalStressItems from './stress_components/physical_item';
 
-const noGutters = {
-    paddingRight: '0',
-    paddingLeft: '0'
-};
-
 const StressList = ({myStress}) => {
   return (
-    <div className="col-sm-8" style={noGutters}>
+    <div className="col-sm-8">
       <h4>Stress Categories</h4>
       <MentalStressItems className="col-sm-8" mental={myStress.mental}/>
       <PhysicalStressItems className="col-sm-8" physical={myStress.physical}/>
