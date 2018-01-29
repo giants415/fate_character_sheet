6420bcd528dd37f1950af7d80026c7acfa13ca3a
Andrew <andrew.vinocur@gmail.com>
2018-01-29 14:32:55 -0800

    removes styling

--- src/components/consequence_list.js
+++ src/components/consequence_list.js
@@ -1,13 +1,8 @@
 import React from 'react';
 
-const noGutters = {
-    paddingRight: '0',
-    paddingLeft: '0'
-};
-
 const ConsequenceList = ({myConsequences}) => {
   return (
-    <div className="col-sm-4" style={noGutters}>
+    <div className="col-sm-4">
       <h4>Consequences</h4>
       <p>Mild: {myConsequences.mild}</p>
       <p>Moderate: {myConsequences.moderate}</p>
