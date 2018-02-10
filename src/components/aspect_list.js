f2955e4e149505851da37d5a9a835007d82bc9da
Andrew <andrew.vinocur@gmail.com>
2018-02-09 17:33:56 -0800

    tables are lame :(

--- src/components/aspect_list.js
+++ src/components/aspect_list.js
@@ -3,16 +3,13 @@ import React from 'react';
 const AspectList = ({myAspects}) => {
   return (
     <div className="col-lg-6">
-      <table>
-        <thead><tr><th>Aspects</th></tr></thead>
-        <tbody>
-          <tr><td>{myAspects[0]}</td></tr>
-          <tr><td>{myAspects[1]}</td></tr>
-          <tr><td>{myAspects[2]}</td></tr>
-          <tr><td>{myAspects[3]}</td></tr>
-          <tr><td>{myAspects[4]}</td></tr>
-        </tbody>
-      </table>
+      <ul>
+        <li>{myAspects[0]}</li>
+        <li>{myAspects[1]}</li>
+        <li>{myAspects[2]}</li>
+        <li>{myAspects[3]}</li>
+        <li>{myAspects[4]}</li>
+      </ul>
     </div>
   )
 };
