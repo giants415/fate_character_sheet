d17c77435011b7c9835c26614f9cf26a57a76280
Andrew <andrew.vinocur@gmail.com>
2018-02-09 17:15:44 -0800

    removes old styling

--- src/components/character_list.js
+++ src/components/character_list.js
@@ -4,16 +4,11 @@ import Name from './character_components/name';
 import Description from './character_components/description';
 import Refresh from './character_components/refresh';
 
-const label = {
-  position: 'relative',
-  left: '15px'
-}
-
 export default class CharacterList extends Component {
   render() {
     return (
       <div className="row">
-        <h3 style={label}>Character</h3>
+        <h3>Character</h3>
         <div className='col-lg-9'>
           <Name />
           <Description />
