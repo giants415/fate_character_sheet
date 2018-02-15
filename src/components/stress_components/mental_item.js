1e5ccb397e671306b9fe7d73d95a03177830cfda
Andrew <andrew.vinocur@gmail.com>
2018-02-14 21:55:35 -0800

    chaanges to stressbox container

--- src/components/stress_components/mental_item.js
+++ src/components/stress_components/mental_item.js
@@ -2,12 +2,10 @@ import React from 'react';
 
 const MentalStressItems = ({mental}) => {
   return (
-    <div>
-      <p>Mental Stress</p>
-      <div className='stress_box'>
-        <p>{mental[0]}</p>
-        <p>{mental[1]}</p>
-      </div>
+    <div className='stress_box'>
+      <h6>Mental Stress</h6>
+      <p>{mental[0]}</p>
+      <p>{mental[1]}</p>
     </div>
   )
 };
