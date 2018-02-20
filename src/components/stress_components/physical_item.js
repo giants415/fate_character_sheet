7989393a849b8f8a15ba08f165bf39dc9feb62e2
Andrew <andrew.vinocur@gmail.com>
2018-02-20 11:42:25 -0800

    replaces divs with stressbox component

--- src/components/stress_components/physical_item.js
+++ src/components/stress_components/physical_item.js
@@ -5,8 +5,7 @@ const PhysicalStressItems = ({physical}) => {
   return (
       <div className='stress_box'>
         <span>Physical Stress</span>
-        <div>{physical[0]}</div>
-        <div>{physical[1]}</div>
+        <StressBox />
       </div>  
   )
 };
