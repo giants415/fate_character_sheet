218ae0e476cb949f1800aacc37defabb37063a95
Andrew <andrew.vinocur@gmail.com>
2018-02-20 11:15:35 -0800

    sets up constructor for class component

--- src/components/stress_box.js
+++ src/components/stress_box.js
@@ -1,3 +1,11 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+class StressBox extends Component {
+  construtor(props) {
+    super(props);
+
+    this.state = { active: 0 };
+  }
+}
 
 export default StressBox;
