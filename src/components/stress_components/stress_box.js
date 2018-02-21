c29bb6ef0efb4ff95d0aa7bda9471762a7f005a3
Andrew <andrew.vinocur@gmail.com>
2018-02-21 10:06:24 -0800

    now able to update state and display, working to target specific div

--- src/components/stress_components/stress_box.js
+++ src/components/stress_components/stress_box.js
@@ -13,22 +13,19 @@ class StressBox extends Component {
   }
 
   onClick(event) {
-    console.log(event.target.state);
+    this.setState({ display: 'X' });
+    console.log(this.state);
   }
 
   render(){
     return (
       <div>
-        <div
-          key={1}
-          onClick={this.onClick}
-          >
+        <div key={1} onClick={this.onClick}>
+          {this.state.display}
         </div>
 
-        <div
-          key={2}
-          onClick={this.onClick}
-          >
+        <div key={2} onClick={this.onClick}>
+          {this.state.display}
         </div>
       </div>
     );
