00311de99d7145c7260c7803d92fdc87e016f16d
Andrew <andrew.vinocur@gmail.com>
2018-02-23 22:23:32 -0800

    creates basic component layout

--- src/components/character_components/stress_list.js
+++ src/components/character_components/stress_list.js
@@ -1,4 +1,20 @@
 import React, {Component} from 'react';
+import StressBox from './stress_components/stress_box';
+
+class StressList extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <div>
+        <StressBox />
+        <StressBox />
+      <div>
+    )
+  }
+}
 
 
 export default StressList;
