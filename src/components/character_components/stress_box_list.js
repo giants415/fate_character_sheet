cda7ae4fc6b7f5a9ae96a7fccfccfc4648d35afe
Andrew <andrew.vinocur@gmail.com>
2018-02-28 18:09:36 -0800

    renames file to avoid confusion with original stress list component

---
+++ src/components/character_components/stress_box_list.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import StressBox from './stress_components/stress_box';
+
+const StressList = () => {
+  return(
+    <div>
+      <StressBox key={1}/>
+      <StressBox key={2}/>
+    <div>
+  )
+};
+
+
+export default StressList;
