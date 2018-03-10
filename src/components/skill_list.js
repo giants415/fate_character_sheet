bc73f12668bfc5cf1386d101b5ccded6a5744e69
Andrew <andrew.vinocur@gmail.com>
2018-03-09 18:50:31 -0800

    adapts skill list for use with skill box

--- src/components/skill_list.js
+++ src/components/skill_list.js
@@ -1,11 +1,6 @@
 import React, {Component} from 'react';
 
-import SuperbSkills from './skill_rank_components/superb_skills';
-import GreatSkills from './skill_rank_components/great_skills';
-import GoodSkills from './skill_rank_components/good_skills';
-import FairSkills from './skill_rank_components/fair_skills';
-import AverageSkills from './skill_rank_components/average_skills';
-
+import SkillBox from './skill_box';
 
 //may be necessary to remove individual skill rank components and instead
 //create a single skill box, similar to the stress box, that will display a chosen skill
@@ -23,11 +18,7 @@ class SkillList extends Component {
     return (
       <div className="col-lg-6 skill_list">
         <h3>Skills</h3>
-        <SuperbSkills/>
-        <GreatSkills/>
-        <GoodSkills/>
-        <FairSkills/>
-        <AverageSkills/>
+          <SkillBox />
       </div>
     );
   }
