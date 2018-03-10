c478fa31f40aed8a083735cdcf82b3ae177417c4
Andrew <andrew.vinocur@gmail.com>
2018-03-09 17:02:49 -0800

    removes superb skills props

--- src/components/skill_rank_components/superb_skills.js
+++ src/components/skill_rank_components/superb_skills.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const SuperbSkills = ({superbSkills}) => {
+const SuperbSkills = () => {
   return (
     <div>
       <p>Superb ( +5 )</p>
       <ul>
-        <li>{superbSkills[0]}</li>
-        <li>{superbSkills[1]}</li>
-        <li>{superbSkills[2]}</li>
-        <li>{superbSkills[3]}</li>
-        <li>{superbSkills[4]}</li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
       </ul>
     </div>
   );
