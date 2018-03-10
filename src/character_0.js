7231c74caa5eddd2586c2cb2c208eab417751ba8
Andrew <andrew.vinocur@gmail.com>
2018-03-09 17:00:28 -0800

    removes placeholders and replaces with list of skills

--- src/character_0.js
+++ src/character_0.js
@@ -5,13 +5,26 @@ const Character_0 = {
     description: 'a cool guy with a disposition',
     refresh: 3,
     aspectList: ['aspect 1', 'aspect 2','aspect 3','aspect 4','aspect 5'],
-    skillList: {
-      superb: ['superb 1', 'superb 2', 'superb 3', 'superb 4', 'superb 5'],
-      great: ['great 1', 'great 2', 'great 3', 'great 4', 'great 5'],
-      good: ['good 1', 'good 2', 'good 3', 'good 4', 'good 5'],
-      fair: ['fair 1', 'fair 2', 'fair 3', 'fair 4', 'fair 5'],
-      average: ['average 1', 'average 2', 'average 3', 'average 4', 'average 5'],
-    },
+    skillList: [
+      'athletics',
+      'burglary',
+      'contacts',
+      'crafts',
+      'deceive',
+      'drive',
+      'empathy',
+      'fight',
+      'investigate',
+      'lore',
+      'notice',
+      'physique',
+      'provoke',
+      'rapport',
+      'resources',
+      'shoot',
+      'stealth',
+      'will'
+    ],
     stuntList: [
       'fast reload - when behind cover, you can reload and shoot using only 1 action',
       'dangerous roads ahead - when driving or riding in a vehicle on dangerous terrian, add +1 to rolls'
