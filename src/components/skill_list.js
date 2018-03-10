c9e9d70d6a7f0e8f286b7463c781bcb17654334a
Andrew <andrew.vinocur@gmail.com>
2018-03-09 17:06:00 -0800

    converts skill list into a class component

--- src/components/skill_list.js
+++ src/components/skill_list.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import SuperbSkills from './skill_rank_components/superb_skills';
 import GreatSkills from './skill_rank_components/great_skills';
@@ -7,17 +7,19 @@ import FairSkills from './skill_rank_components/fair_skills';
 import AverageSkills from './skill_rank_components/average_skills';
 
 
-const SkillList = ({mySkills}) => {
-  return (
-    <div className="col-lg-6 skill_list">
-      <h3>Skills</h3>
-      <SuperbSkills superbSkills={mySkills.superb}/>
-      <GreatSkills greatSkills={mySkills.great}/>
-      <GoodSkills goodSkills={mySkills.good}/>
-      <FairSkills fairSkills={mySkills.fair}/>
-      <AverageSkills AverageSkills={mySkills.average}/>
-    </div>
-  );
+class SkillList extends Component {
+  render(){
+    return (
+      <div className="col-lg-6 skill_list">
+        <h3>Skills</h3>
+        <SuperbSkills superbSkills={mySkills.superb}/>
+        <GreatSkills greatSkills={mySkills.great}/>
+        <GoodSkills goodSkills={mySkills.good}/>
+        <FairSkills fairSkills={mySkills.fair}/>
+        <AverageSkills AverageSkills={mySkills.average}/>
+      </div>
+    );
+  }
 }
 
 export default SkillList;
