c9fb5d5282680f2110b678c501b2d80e6d797565
Andrew <andrew.vinocur@gmail.com>
2018-03-09 19:01:47 -0800

    adds to skill box to apply style

--- src/components/skill_box.js
+++ src/components/skill_box.js
@@ -1,7 +1,18 @@
 import React, {Component} from 'react';
 
 class SkillBox extends Component {
+  constructor(props) {
+    super(props);
 
+  }
+
+  render(){
+    return(
+      <div className="skill_box">
+        <span></span>
+      </div>
+    );
+  }
 }
 
 export default SkillBox;
