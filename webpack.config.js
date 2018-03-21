b35afc3eefc3c425034f3b73d3e8672c4f6c15b3
Andrew <andrew.vinocur@gmail.com>
2018-03-21 09:58:31 -0700

    reorders for heroku testing

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ var path = require('path');
 
 module.exports = {
   entry: [
-    path.resolve(__dirname, 'src') + '/index.js',
-    'webpack-dev-server/client?http://0.0.0.0:80'
+    'webpack-dev-server/client?http://0.0.0.0:80',
+    path.resolve(__dirname, 'src') + '/index.js'
   ],
   output: {
       path: path.resolve(__dirname, 'dist') + '/app',
